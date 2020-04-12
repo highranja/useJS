2de062fd434e47f1bd84b67c61f9515551cb6316
highranja <57704334+highranja@users.noreply.github.com>
2020-04-12 22:10:26 +0900

    = bug , calculation 80%

--- calculator/app.js
+++ calculator/app.js
@@ -4,103 +4,86 @@ const
     screen = wrap.querySelector('.screen'),
     formula = screen.querySelector('.formula'),
     result = screen.querySelector('.result'),
+
     // 계산기 영역
     calculator = wrap.querySelector('table'),
+    npSelect = calculator.querySelector('.npSelect'),
     opr = calculator.querySelectorAll('.opr'),    
     num = calculator.querySelectorAll('.num'),
+    reset = calculator.querySelector('#reset'),
     rs = calculator.querySelector('.rs');
 
-let 
-    REALNUM='',
-    NUMBOX=[],
-    OPRBOX=[];
-let cal = 0;
+let cal = {
+    rs : '',
+    bool : true, // 숫자 lock
+    opr : false   //연산자 lock
+}; //결과
 
 
 
-function calculation(NUMBOX,OPRBOX,REALNUM){
-    debugger;
 
-    for(let i = 0; i < NUMBOX.length; i++){
-        if(!NUMBOX[i+1]){
-            switch (OPRBOX[i]){
-                case '+':
-                    cal += (NUMBOX[i] + NUMBOX[i+1]);
-                    break;
-                case '-':
-                    cal += (NUMBOX[i] - NUMBOX[i+1]);
-                    break;
-                case '*':
-                    cal += (NUMBOX[i] * NUMBOX[i+1]);
-                    break;
-                case '/':
-                    cal += (NUMBOX[i] / NUMBOX[i+1]);
-                    break;
-            }
-            
-        }else{            
-            switch (OPRBOX[i]){
-                case '+':
-                    cal += (NUMBOX[i] + (REALNUM*1));
-                    break;
-                case '-':
-                    cal += (NUMBOX[i] - (REALNUM*1));
-                    break;
-                case '*':
-                    cal += ( NUMBOX[i] * (REALNUM*1) );
-                    break;
-                case '/':
-                    cal += (NUMBOX[i] / (REALNUM*1));
-                    break;
-            }
-        }
+
+
+function a(){
+    if(result.firstElementChild.textContent.includes('-')){
+        result.firstElementChild.textContent=result.firstElementChild.textContent.replace('-','+');
     }
+}
+npSelect.addEventListener('click',a);
 
 
-
+// 계산함수 
+function calculation( txt ){
+    cal.bool = false;
+    cal.opr = true;
+    return new Function('return ' + txt)();
 }
 
 
 
 
-
-
-
-
-
-
-
+//숫자 입력
 num.forEach( (item,idx) =>{
-    item.addEventListener('click', ()=>{        
-
-
-
-        REALNUM += item.textContent; 
+    item.addEventListener('click', ()=>{      
+        if(!cal.bool) return;
+        cal.opr = true;
         result.firstElementChild.textContent += item.textContent;   
     });
 });
 
+//연산자 입력
 opr.forEach( (item,idx) =>{
     item.addEventListener('click', ()=>{  
-        NUMBOX.push(REALNUM*1); //숫자를 넣는다
-        OPRBOX.push(item.textContent);        
-        REALNUM = '';
+        if(!cal.opr){
+            return;
+        }else{
+            cal.opr = false;
+        }
 
+        if(!cal.bool){
+            cal.bool = true;
+        }
 
-        result.firstElementChild.textContent += item.textContent;    
+        result.firstElementChild.textContent += item.textContent;            
     });
 });
 
-rs.addEventListener('click', ()=>{   
-    calculation( NUMBOX, OPRBOX, REALNUM );
-    result.firstElementChild.textContent = cal;
-    reset();    
+// 결과 출력
+rs.addEventListener('click', ()=>{      
+    if(result.firstElementChild.textContent === '')return;
+    
+
+    cal.rs = Math.round(calculation(result.firstElementChild.textContent) * 100)/100;
+    formula.firstElementChild.textContent = result.firstElementChild.textContent;
+    result.firstElementChild.textContent = cal.rs ;  
 });
 
 
-
-function reset(){
-    NUMBOX = [cal];
-    OPRBOX = []; 
-    cal = 0;   
-}
+// 초기화
+reset.addEventListener('click',function(){
+    formula.firstElementChild.textContent =
+    result.firstElementChild.textContent = 
+    cal.rs  = '';
+    cal.bool = true;
+    cal.opr = false;
+});
