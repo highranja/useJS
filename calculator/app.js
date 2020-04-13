239d961fe6acc5542350ff4618c47ddb072c096a
highranja <57704334+highranja@users.noreply.github.com>
2020-04-13 22:59:45 +0900

    prev final, need light / dark theme

--- calculator/app.js
+++ calculator/app.js
@@ -8,82 +8,95 @@ const
     // 계산기 영역
     calculator = wrap.querySelector('table'),
     npSelect = calculator.querySelector('.npSelect'),
+    percent = calculator.querySelector('.percent'),
+    float = calculator.querySelector('.float'),
     opr = calculator.querySelectorAll('.opr'),    
     num = calculator.querySelectorAll('.num'),
     reset = calculator.querySelector('#reset'),
     rs = calculator.querySelector('.rs');
 
 let cal = {
-    rs : '',
-    bool : true, // 숫자 lock
-    opr : false   //연산자 lock
-}; //결과
+    rs : '', // 결과
+    num : true, // 숫자 lock
+    opr : false,   //연산자 lock
+    float: true, // 소수점 Lock
+};
 
-
-
-
-
-
-function a(){
-    if(result.firstElementChild.textContent.includes('-')){
-        result.firstElementChild.textContent=result.firstElementChild.textContent.replace('-','+');
+// 음수 양수 변환
+function npSelector(){
+    if(result.firstElementChild.textContent.charAt(0) === '-'){
+        result.firstElementChild.textContent = result.firstElementChild.textContent.replace('-','');
+    }else{
+        result.firstElementChild.textContent = '-' + result.firstElementChild.textContent;
     }
 }
-npSelect.addEventListener('click',a);
-
-
-// 계산함수 
-function calculation( txt ){
-    cal.bool = false;
-    cal.opr = true;
-    return new Function('return ' + txt)();
+// 퍼센트 변환
+function percenter(){
+    if(result.firstElementChild.textContent.includes('+' || '-' || '*' || '/') 
+    || result.firstElementChild.textContent === ''){
+        return;
+    }else{
+        cal.rs =calculation(result.firstElementChild.textContent)*0.01;
+        result.firstElementChild.textContent = cal.rs;
+    }
 }
+// 계산함수 
+function calculation( txt ){    
+    cal.num = false;
+    cal.opr = true;
+    cal.float = false;
+    return new Function('return ' + txt)();      
+}
+// 결과 출력함수
+function resultShow(){
+    if(!cal.opr) return;
+    cal.rs = Math.round(calculation(result.firstElementChild.textContent) * 100)/100;
+    formula.firstElementChild.textContent = result.firstElementChild.textContent;
+    result.firstElementChild.textContent = cal.rs ;    
+} 
+// 초기화 함수
+function resetAll(){
+    formula.firstElementChild.textContent =
+    result.firstElementChild.textContent = 
+    cal.rs  = '';
 
-
+    cal.num = true;
+    cal.opr = false;
+    cal.float = false;
+}
 
 
 //숫자 입력
 num.forEach( (item,idx) =>{
-    item.addEventListener('click', ()=>{      
-        if(!cal.bool) return;
-        cal.opr = true;
+    item.addEventListener('click',function(){    
+        if(!cal.num) return;
         result.firstElementChild.textContent += item.textContent;   
+        cal.opr = true;            
     });
+});
+
+float.addEventListener('click',function(){
+    if(!cal.float) return;
+    
+    result.firstElementChild.textContent += 
+    (result.firstElementChild.textContent === '') 
+    ? '0'+this.textContent : this.textContent;
+    cal.float = false;    
 });
 
 //연산자 입력
 opr.forEach( (item,idx) =>{
-    item.addEventListener('click', ()=>{  
-        if(!cal.opr){
-            return;
-        }else{
-            cal.opr = false;
-        }
-
-        if(!cal.bool){
-            cal.bool = true;
-        }
-
-        result.firstElementChild.textContent += item.textContent;            
+    item.addEventListener('click',function(){   
+        if(!cal.opr) return;
+        result.firstElementChild.textContent += item.textContent; 
+        cal.num = true;
+        cal.opr = false;
+        cal.float = true;                        
     });
 });
 
-// 결과 출력
-rs.addEventListener('click', ()=>{      
-    if(result.firstElementChild.textContent === '')return;
-    
 
-    cal.rs = Math.round(calculation(result.firstElementChild.textContent) * 100)/100;
-    formula.firstElementChild.textContent = result.firstElementChild.textContent;
-    result.firstElementChild.textContent = cal.rs ;  
-});
-
-
-// 초기화
-reset.addEventListener('click',function(){
-    formula.firstElementChild.textContent =
-    result.firstElementChild.textContent = 
-    cal.rs  = '';
-    cal.bool = true;
-    cal.opr = false;
-});
+percent.addEventListener('click',percenter);
+npSelect.addEventListener('click',npSelector);
+rs.addEventListener('click',resultShow); // 결과 출력
+reset.addEventListener('click',resetAll); // 초기화
