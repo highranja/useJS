5cf2e1a318f843b0afc65bbd56cc9453f47bbcfa
highranja <57704334+highranja@users.noreply.github.com>
2020-04-24 19:59:29 +0900

    Add function ( save , delete, load )

--- toDoList/app.js
+++ toDoList/app.js
@@ -3,24 +3,28 @@
 const
     ToDo = document.querySelector('.wrap .container .todo'),
     searchBox = ToDo.querySelector('.todo-search'),
+    searchForm = searchBox.querySelector('#search'), // 검색 칸
     todoBox = ToDo.querySelector('.todo-contents'),
-    todoAddBox = todoBox.querySelector('.todo-add'),
+    todoAddBox = todoBox.querySelector('.todo-add'), // to do list 추가 및 열람 칸
 
-    AddForm = todoAddBox.querySelector('#add'),
-    todoInput = AddForm.querySelector('.add-input'),
+    addForm = todoAddBox.querySelector('#add'),
+    todoInput = addForm.querySelector('.add-input'),
     todoListBox = todoBox.querySelector('.todo-list');
+     
+const DOID = 'doId';
+let itemDataBase = [];
 
 
 
-
-
-AddForm.addEventListener('submit',function(e){
-    let txt;
+addForm.addEventListener('submit',function(e){
     e.preventDefault();
+    let txt;
     txt = todoInput.value;
-    todoInput.value = '';
     todoListBox.appendChild(createList(`${txt}`));
 
+    todoInput.value = '';
+    saveToDo();
+    deleteToDo();
 });
 
 // 리스트 todo 추가 함수
@@ -54,3 +58,46 @@ const createList = (txt) =>{
     return item;
 
 }
+
+// list delete 함수
+const deleteToDo = () => {
+    let deleteBox = todoListBox.querySelectorAll('button');
+    deleteBox.forEach((item,idx) => {
+        item.addEventListener('click',function(){
+            if(this.parentNode.firstElementChild.checked){ // input[checkbox]가 체크되어있으면
+                this.parentNode.remove();
+                saveToDo();
+            }                   
+        });
+    });        
+}
+
+// list save 함수
+const saveToDo = () => {
+    let toDoItem = todoListBox.querySelectorAll('.todo-item');
+    itemDataBase = [];
+    toDoItem.forEach((item,idx) => {
+        let desc = {
+            id : idx,
+            content : item.innerText
+        }
+        itemDataBase.push(desc);        
+    });
+    localStorage.setItem(DOID,JSON.stringify(itemDataBase));
+}
+
+// 새로고침했을 때 list 
+function loadToDo(){
+    const loadedToDo = localStorage.getItem(DOID);
+    if(loadedToDo){
+        const parsedToDos = JSON.parse(loadedToDo);
+        // 다시 끌고올때는 오브젝트로 변환
+        parsedToDos.forEach(function(todo){
+            todoListBox.appendChild(createList(`${todo.content}`));
+        });
+    } 
+    deleteToDo();
+}
+window.onload = loadToDo();
+
+// todo 검색 및 구현
