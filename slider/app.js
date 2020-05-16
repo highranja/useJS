0f6ad8941c89e922d954f5d466bd611b3a37b282
highranja <57704334+highranja@users.noreply.github.com>
2020-05-16 21:52:10 +0900

    Finish slider

--- slider/app.js
+++ slider/app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 "use strict";
 
 const 
@@ -75,72 +74,3 @@ function barMoveOnSlide() {
 	bar.style.width = `${barPercent}%`;
 }
 
-=======
-"use strict";
-
-const slideContainer = document.querySelector(".slide-container"),
-	slideList = slideContainer.querySelector(".slide-list"),
-	slide = slideList.querySelectorAll(".slide"),
-	barBox = document.querySelector(".slide-bar"),
-	prev = barBox.querySelector("#prev"),
-	next = barBox.querySelector("#next"),
-	bar = barBox.querySelector(".bar");
-
-let move = 0,
-	grabFrom = false,
-	count = 0;
-
-slideContainer.addEventListener("mousedown", (e) => {
-	grabFrom = true;
-	move += e.clientX;
-});
-
-slideContainer.addEventListener("mousemove", (e) => {
-	if (grabFrom) {
-		slideMove(e);
-	}
-});
-
-slideContainer.addEventListener("mouseup", (e) => {
-	grabFrom = false;
-	move -= e.clientX;
-	slideSetOnLine();
-});
-
-function slideMove(e) {
-	const currentX = slideList.offsetLeft;
-}
-
-function slideSetOnLine() {
-	console.log("놨다요놈");
-	move = 0;
-}
-
-prev.addEventListener("click", () => {
-	if (count < 1) return;
-	count--;
-	btnSlide();
-});
-next.addEventListener("click", () => {
-	if (count > slide.length - 4) return;
-	count++;
-	console.log(count);
-	btnSlide();
-});
-
-function btnSlide() {
-	const slideWidth = slide[0].offsetWidth;
-	const sliderWidth = slideList.offsetWidth;
-	const currentSlideMove = count * slideWidth + 30 * count;
-
-	slideList.style.left = `-${currentSlideMove}px`;
-	barMoveOnSlide();
-}
-
-function barMoveOnSlide() {
-	const maxNumber = slide.length - 3;
-	const barPercent = (100 / maxNumber) * count;
-
-	bar.style.width = `${barPercent}%`;
-}
->>>>>>> e30aa79833e20636d5e992d035bfb1a92e2a9083
